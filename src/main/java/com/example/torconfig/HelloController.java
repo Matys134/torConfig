f9d824213b06b7b8a77fc779e512f825baabe8b5
Matys <52925278+Matys134@users.noreply.github.com>
2023-04-09 21:03:25 +0200

    Moar Buttons (And maybe working this time)

--- src/main/java/com/example/torconfig/HelloController.java
+++ src/main/java/com/example/torconfig/HelloController.java
@@ -1,14 +1,49 @@
 package com.example.torconfig;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    public Button installTor;
+    public Text isTorInstalled;
+    public Button checkTor;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void InstallTor() {
+        //install tor on ubuntu
+try {
+            ProcessBuilder processBuilder = new ProcessBuilder("sudo", "apt-get", "install", "tor");
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                System.out.println("Tor is installed.");
+            } else {
+                System.out.println("Tor is not installed.");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @FXML
+    void checkIfTorInstalled() {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("which", "tor");
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                System.out.println("Tor is installed.");
+            } else {
+                System.out.println("Tor is not installed.");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
